9bae4c56becf652265f8ea2292eb77a842582fa2
chandini2136 <gangavaramchandini@gmail.com>
2024-07-10 13:09:37 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def ROSEP1(request):
+    return render(request,'ROSEP1.html')
+
+
+def SURYAD(request):
+    return render(request,'SURYAD.html')
+
+
+def animation(request):
+    return render(request,'animation.html')
+
+
+def ball(request):
+    return render(request,'ball.html')
+
+
+def door(request):
+    return render(request,'door.html')
+
+
+def frame(request):
+    return render(request,'frame.html')
+
+
+def frame1(request):
+    return render(request,'frame1.html')
+
+
+def window(request):
+    return render(request,'window.html')
+
+
+def image(request):
+    return render(request,'image.html')
+
+
+def table(request):
+    return render(request,'table.html')
+
+
